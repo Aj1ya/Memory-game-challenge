ff08f8cc0704c3363c99d5a924f6db7813f2e4fd
Ajaya <ajayamaharjan93@gmail.com>
2021-12-12 15:11:30 +1100

    Update: added selection screen with js to toggle on click

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,25 @@
 // use strict es6;
+var selectGame = []; 
+    var btnNumbers = $("#numbers");
+    var btnIcons = $('#icons');
+    selectGame.push(btnNumbers.attr('id'), btnIcons.attr('id'));
+    console.log(selectGame);
+    $(document).ready(function() {
+        $("#numbers, #icons, #one, #two, #three, #four, #grid-one, #grid-two, #start-game-btn").click(function() {
+            $(this).toggleClass("selected");
+        });
+        $("#numbers, #icons, #one, #two, #three, #four, #grid-one, #grid-two, #start-game-btn").hover(function() {
+            $(this).toggleClass("hovered");
+        });
+    });
 
-console.log("connected");
+    $(document).ready(function(){
+        $("#numbers, #icons, #one, #two, #three, #four, #grid-one, #grid-two, #start-game-btn").click(function() {
+            for(var i = 0; i < selectGame.length; i++) {
+                if($(this).attr('id') === selectGame[i]){
+                    console.log($(this).attr('id'));
+                }
+            }
+        });  
+       
+    });
