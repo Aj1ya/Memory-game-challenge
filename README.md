36c538f00b577b103621231bfc3756c7b753ebb8
Ajaya Maharjan <ajayamaharjan93@gmail.com>
2023-03-29 21:01:14 +1100

    Update: Logic added for funtionality

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Memory-game-challenge
 This is a memory game solution based on frontend mentor website challenge.
+
+
+version 1.01
+
+Updates:
+
+1. Updated the css to be in assests folder
+2. Logic added to the button clicks and functionality added for numbers and icons game.
+3. Styling adjustment for inner game board
--- scripts/script.js
+++ scripts/script.js
@@ -4,23 +4,62 @@ var selectGame = [];
     var btnNumbers = $("#numbers");
     var btnIcons = $('#icons');
     selectGame.push(btnNumbers.attr('id'), btnIcons.attr('id'));
-    console.log(selectGame);
-    $(document).ready(function() {
-        $("#numbers, #icons, #one, #two, #three, #four, #grid-one, #grid-two, #start-game-btn").click(function() {
-            $(this).toggleClass("selected");
-        });
-        $("#numbers, #icons, #one, #two, #three, #four, #grid-one, #grid-two, #start-game-btn").hover(function() {
-            $(this).toggleClass("hovered");
-        });
-    });
 
-    $(document).ready(function(){
-        $("#numbers, #icons, #one, #two, #three, #four, #grid-one, #grid-two, #start-game-btn").click(function() {
-            for(var i = 0; i < selectGame.length; i++) {
-                if($(this).attr('id') === selectGame[i]){
-                    console.log($(this).attr('id'));
-                }
+
+    $(document).ready(function() {
+        var gameQuery = new Array(3);
+
+        //! select the game type allowing only one selection for each option
+        $( "#numbers, #icons ").on( "click", function() {
+            $(this).toggleClass("selected");
+            $(this).siblings().removeClass("selected");
+            if( $(this).hasClass("selected") ) {
+                gameQuery[0] = $(this).attr('id');
+                // gameType = $(this).attr('id');
             }
-        });  
-       
+        } );
+
+        $( "#one, #two, #three, #four ").on( "click", function() {
+            $(this).toggleClass("selected");
+            $(this).siblings().removeClass("selected");
+            if( $(this).hasClass("selected") ) {
+                // numberOfPlayers = $(this).attr('id');
+                gameQuery[1] = $(this).attr('id');
+            }
+        } );
+
+        $( " #grid-one, #grid-two ").on( "click", function() {
+            $(this).toggleClass("selected");
+            $(this).siblings().removeClass("selected");
+            if( $(this).hasClass("selected") ) {
+                // gameGrid = $(this).attr('id');
+                gameQuery[2] = $(this).attr('id');
+            }
+        } );
+
+        //! check if the user has selected all the options
+        $( "#start-game-btn" ).on( "click", function() {
+            if(gameQuery.length === 3){
+                if( gameQuery[0] == "icons"  ){
+                    startGameIcons();
+                }
+                else{
+                    startGameNumbers();
+                }
+            };
+        } );
+
+        function startGameIcons(){
+            console.log( "icons" );
+            $( ".icons" ).addClass("show");
+            $( ".numbers" ).addClass("hide");
+            $( ".start-up" ).addClass("hide");
+        }
+
+        function startGameNumbers(){
+            console.log( "numbers" );
+            $( ".numbers" ).addClass("show");
+            $( ".icons" ).addClass("hide");
+            $( ".start-up" ).addClass("hide");
+        }
     });
